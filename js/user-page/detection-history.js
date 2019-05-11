8fba50f6e8c00bfedbd96a17c9a8104d7ac93936
renchongwei123 <45593080+renchongwei123@users.noreply.github.com>
2019-05-11 08:30:41 +0800

    修改ajax请求

--- js/user-page/detection-history.js
+++ js/user-page/detection-history.js
@@ -1,25 +1,36 @@
 function getData(url, data) {
-    return fetch(url)
+    return fetch(url, data)
         .then(
-            (response) => {
-                if (!response.ok) { 
-                    handleError(response);
-                    return 0;
-                };
-                return response.json();
+            //如果响应成功，成为resolveed状态
+            response => {
+                //判断响应是否返回成功的状态码
+                if (!response.ok) {
+                    const error = new Error(`请求失败! 状态码: ${response.status}, 失败信息: ${response.statusText}`);
+                    error.response = response;
+                    return Promise.reject(error);
+                } else {
+                    return response.json();
+                }
             },
-            () => { 
+            //如果响应失败，成为rejected状态
+            () => {
+                const error = new Error('网络错误');
                 alert('网络错误！');
+                throw error;
             }
         )
 }
 getData('https://www.easy-mock.com/mock/5c5414c1deae1e58bc943374/BnoeDetection/history-detection-result#!method=get')
-    .then(data => console.log(data))
-    .catch(error => {throw new Error(error)});
+    .then(data => console.log(data), error => { handleError(error) })
+//处理响应异常的函数   
+function handleError(error) {
+    //do something to handle error
+    console.log(error);
 
-function handleError(response){
-    console.log(response);
 }
+
+
+
 
 require.config({
     paths: {
